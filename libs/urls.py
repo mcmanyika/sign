5aeaf783be4a900bcc29bc72c1f516fc5b5f2f14
mcmanyika <mcmanyika.com>
2023-01-10 07:31:26 -0600

    changed index url

--- libs/urls.py
+++ libs/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index.as_view(), name='index'),
+
+    path('', showresults, name='search_sign'),
+    path('index', index.as_view(), name='index'),
     path('search/', SearchView.as_view(), name='search'),
-    path('search-sign', showresults, name='search_sign'),
 ]
